7f9ce7e05845b7c64e475e98279fc2565efbb46d
Hitesh <hiteshmalhotra099@gmail.com>
2023-10-13 00:41:10 +0530

    Add LinkedListDemo.java

--- src/LinkedList/LinkedListDemo.java
+++ src/LinkedList/LinkedListDemo.java
@@ -75,17 +75,17 @@ public void addLast(int value)
 		nn.next=temp2;
 		size++;
 	}
-	public node getAt(int idx)
-	{
-		node temp=head;
-		int cnt=0;
-		while(cnt<idx)
-		{
-			temp=temp.next;
-			cnt++;
-		}
-		return temp;
-	}
+//	public node getAt(int idx)
+//	{
+//		node temp=head;
+//		int cnt=0;
+//		while(cnt<idx)
+//		{
+//			temp=temp.next;
+//			cnt++;
+//		}
+//		return temp;
+//	}
 //	public void removeFirst()
 //	{
 //		if(head==null)
@@ -95,7 +95,7 @@ public void addLast(int value)
 //	head=head.next;
 //	size--;
 //	}
-	public void removeLast()
+	public void removeLast2()
 	{
 		if(head==null)
 		{
@@ -106,26 +106,26 @@ public void addLast(int value)
 		tail=temp;
 	size--;
 	}
-	public void removeAt(int idx)
-	{
-	if(idx==0)
-	{
-		removeFirst();
-		return;
-	}
-	if(idx==size-1)
-	{
-		removeLast();
-	}
-	if(idx<0||idx>=size)
-	{
-		return;
-	}
-	node temp=getAt(idx-1);
-	node next=temp.next;
-//	temp.next=temp.next.next;
-	temp.next=next.next;
-	}
+//	public void removeAt(int idx)
+//	{
+//	if(idx==0)
+//	{
+//		removeFirst();
+//		return;
+//	}
+//	if(idx==size-1)
+//	{
+//		removeLast();
+//	}
+//	if(idx<0||idx>=size)
+//	{
+//		return;
+//	}
+//	node temp=getAt(idx-1);
+//	node next=temp.next;
+////	temp.next=temp.next.next;
+//	temp.next=next.next;
+//	}
 	public void rotate1()
 	{
 		int i=0;
@@ -165,8 +165,98 @@ public void addLast(int value)
 //	g25
 	public void removeFirst()
 	{
+		if(head==null)
+		{
+			return ;
+		}
 		head=head.next;
+		size--;
+	}
+	public void removeLast()
+	{
+		node temp=getAt(size-2);
+	temp.next=null;
+	tail=temp;
+			
+	}
+	public node getAt(int idx)
+	{
+		if(idx>=size||idx<0)
+		{
+			
+			return null;
+		}
+		int cnt=0;
+		node temp=head;
+		while(cnt<idx)
+		{
+			cnt++;
+			temp=temp.next;
+			
+		}
+		return temp;
+	}
+	public void removeAt(int idx)
+	{
+		if(idx==0)
+		{
+			//removeFirst ki baat ho rhe h
+			removeFirst();
+			return;
+		}
+		if(idx==size-1)
+		{
+			//removeLast ki baat ho rhe h
+			removeLast();
+			return;
+		}
+		if(idx<0||idx>=size)
+		{
+			return;
+		}
+		//iska mtlb yeh h ki size jo h vo 0 >&& size<size-1;
+	node temp=getAt(idx-1);
+	node next=temp.next;
+	temp.next=next.next;
+	}
+	public void rotate()
+	{
+		Stack<node>s=new Stack();
+		node temp=head;
+		while(temp!=null)
+		{
+			s.push(temp);
+			temp=temp.next;
+		}
+		temp=head;//temp=1k;
+		//top of stack h 5k;
+		int cnt=0;
+		while(cnt<size/2)
+		{
+			node next=s.pop();//5k
+			int val=temp.val;
+			temp.val=next.val;
+			next.val=val;
+			
+			cnt++;
+		}
+	}
+		public node rotate5(node cur,int cnt)
+		{
+			if(cur==null)
+			{
+				return head;
+			}
+			node prev=rotate5(cur.next,cnt+1);
+		//swap;
+			if(cnt<size/2)
+			{
+			int val=prev.val;
+			prev.val=cur.val;
+			cur.val=val;
+			return prev.next;
+			}
+			}
 		
 	}
-	
 }
